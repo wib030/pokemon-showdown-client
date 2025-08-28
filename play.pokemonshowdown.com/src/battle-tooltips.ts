f78109bdae0edb14917ddfe7140bb434b1a9225a
wib030 <localizedlyss@gmail.com>
2025-08-28 11:53:13 +1000

    Revert "remove this for testing"
    This reverts commit d956d437db50d26c8a6a5c619d1c183b6ff1ec77.

--- play.pokemonshowdown.com/src/battle-tooltips.ts
+++ play.pokemonshowdown.com/src/battle-tooltips.ts
@@ -1761,7 +1761,45 @@ export class BattleTooltips {
 		let pokemon = value.pokemon;
 		let moveType = move.type;
 		
-		
+		if (move.id === 'judgment' || move.id === 'fling') {
+			if (value.tryItem('Fist Plate')) {
+				moveType = 'Fighting';
+			} else if (value.tryItem('Sky Plate')) {
+				moveType = 'Flying';
+			} else if (value.tryItem('Toxic Plate')) {
+				moveType = 'Poison';
+			} else if (value.tryItem('Earth Plate')) {
+				moveType = 'Ground';
+			} else if (value.tryItem('Stone Plate')) {
+				moveType = 'Rock';
+			} else if (value.tryItem('Insect Plate')) {
+				moveType = 'Bug';
+			} else if (value.tryItem('Spooky Plate')) {
+				moveType = 'Ghost';
+			} else if (value.tryItem('Iron Plate')) {
+				moveType = 'Steel';
+			} else if (value.tryItem('Flame Plate')) {
+				moveType = 'Fire';
+			} else if (value.tryItem('Splash Plate')) {
+				moveType = 'Water';
+			} else if (value.tryItem('Meadow Plate')) {
+				moveType = 'Grass';
+			} else if (value.tryItem('Zap Plate')) {
+				moveType = 'Electric';
+			} else if (value.tryItem('Mind Plate')) {
+				moveType = 'Psychic';
+			} else if (value.tryItem('Icicle Plate')) {
+				moveType = 'Ice';
+			} else if (value.tryItem('Draco Plate')) {
+				moveType = 'Dragon';
+			} else if (value.tryItem('Dread Plate')) {
+				moveType = 'Dark';
+			} else {
+				moveType = (move.id === 'judgment') ? 'Normal' : 'Dark';
+			}
+		} else if (value.tryAbility('Normalize')) {
+			moveType = 'Normal';
+		}
 		if (move.id === 'hiddenpower') {
 			moveType = pokemon.hpType || 'Dark';
 		}
