d7f00b6275683caa3ee60848e529309a2281c5d0
wib030 <localizedlyss@gmail.com>
2025-09-15 09:40:30 +1000

    tries this

--- play.pokemonshowdown.com/src/battle-dex-data.ts
+++ play.pokemonshowdown.com/src/battle-dex-data.ts
@@ -631,8 +631,8 @@ export const BattlePokemonIconIndexes: { [id: string]: number } = {
 	ramnarokradiant: 1512 + 79,
 	
 	// New icons
-	wibblypuff: 1597 + 0,
-	megamarill: 1597 + 1,
+	wibblypuff: 1596 + 0,
+	megamarill: 1596 + 1,
 };
 
 export const BattlePokemonIconIndexesLeft: { [id: string]: number } = {
