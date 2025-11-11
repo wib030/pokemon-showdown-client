abee295e743843805180f101be65d7da407ea592
wib030 <localizedlyss@gmail.com>
2025-11-11 12:33:02 +1100

    try to implement custom cries this way

--- play.pokemonshowdown.com/src/battle-dex.ts
+++ play.pokemonshowdown.com/src/battle-dex.ts
@@ -635,7 +635,35 @@ export const Dex = new class implements ModdedDex {
 
 		if (miscData.num !== 0 && miscData.num > -5000) {
 			let baseSpeciesid = toID(species.baseSpecies);
-			spriteData.cryurl = 'audio/cries/' + baseSpeciesid;
+			switch (baseSpeciesid) {
+				case 'wibblypuff':
+					spriteData.cryurl = 'audio/cries/jigglypuff';
+					break;
+				case 'megamarill':
+					spriteData.cryurl = 'audio/cries/marill';
+					break;
+				case 'giratinaantibug':
+				case 'giratinaantidark':
+				case 'giratinaantidragon':
+				case 'giratinaantielectric':
+				case 'giratinaantifighting':
+				case 'giratinaantifire':
+				case 'giratinaantiflying':
+				case 'giratinaantighost':
+				case 'giratinaantigrass':
+				case 'giratinaantiground':
+				case 'giratinaantiice':
+				case 'giratinaantipoison':
+				case 'giratinaantipsychic':
+				case 'giratinaantirock':
+				case 'giratinaantisteel':
+				case 'giratinaantiwater':
+					spriteData.cryurl = 'audio/cries/giratina';
+					break;
+				default:
+					spriteData.cryurl = 'audio/cries/' + baseSpeciesid;
+					break;
+			}
 			let formeid = species.formeid;
 			if (species.isMega || formeid && (
 				formeid === '-crowned' ||
